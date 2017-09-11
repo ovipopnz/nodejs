3cfd651d9e8c436ad21b0c486b4c937d52e378f4
Ovi <ovi@pops.net.nz>
2017-09-12 11:39:02 +1200

    simpler structure

--- flatten.js
+++ flatten.js
@@ -5,8 +5,10 @@ var result = "";
 hosts.all.forEach(function (env) {
     ["n1", "n2"].forEach(function (node) {
         env[node].forEach(function (host) {
-            result = result + "\n" + host.host + "\t";
-            host.alias.forEach(function (alias) {
+            var ip = host.split(' ')[0];
+            var aliases = host.split(' ')[1];
+            result = result + "\n" + ip + "\t";
+            aliases.split(',').forEach(function (alias) {
                 result = result + alias + "." + node + "." + env.name + " ";
             })
         });
